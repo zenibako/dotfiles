ecfc9a29e3891d2a1d345476d10d2ac93fb834bd
Chandler Anderson <chanderson90@proton.me>
2025-09-11 07:29:46 -0400

    feat: add pkl lua plugin

---
+++ nvim/default/lua/plugins/pkl.lua
@@ -0,0 +1,30 @@
+return {
+  "apple/pkl-neovim",
+  lazy = true,
+  ft = "pkl",
+  dependencies = {
+    {
+      "nvim-treesitter/nvim-treesitter",
+      build = function(_)
+        vim.cmd("TSUpdate")
+      end,
+    },
+    "L3MON4D3/LuaSnip",
+  },
+  build = function()
+    require('pkl-neovim').init()
+
+    -- Set up syntax highlighting.
+    vim.cmd("TSInstall pkl")
+  end,
+  config = function()
+    -- Set up snippets.
+    require("luasnip.loaders.from_snipmate").lazy_load()
+
+    -- Configure pkl-lsp
+    vim.g.pkl_neovim = {
+      start_command = { "java", "-jar", vim.fn.expand("$HOME/.local/share/nvim/mason/packages/pkl-lsp/pkl-lsp-0.4.0.jar") },
+      pkl_cli_path = "/opt/homebrew/bin/pkl",
+    }
+  end,
+}
