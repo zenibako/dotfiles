13fe42d9396b618840d04aa3c7feba92d90bf67b
Chandler Anderson <chanderson90@proton.me>
2026-07-11 11:17:03 -0700

    feat(opencode): add adrafinil hooks

--- src/opencode/plugins/adrafinil.ts
+++ src/opencode/plugins/adrafinil.ts
@@ -1,7 +1,29 @@
+const EVENT_ACQUIRE = [
+  "session.status",
+  "session.updated",
+  "session.created",
+]
+
+const EVENT_RELEASE = [
+  "session.idle",
+]
+
 export const Adrafinil = async ({ $ }) => {
   return {
     event: async ({ event }) => {
-      if (event.type === "session.created") await $`"/Applications/Adrafinil.app/Contents/Helpers/adrafinil" acquire ${event.properties.info.id} --tool opencode`
+      let adrafinilAction
+      if (EVENT_ACQUIRE.includes(event.type)) {
+        adrafinilAction = "acquire"
+      }
+      else if (EVENT_RELEASE.includes(event.type)) {
+        adrafinilAction = "release"
+      }
+
+      if (!adrafinilAction) {
+        return
+      }
+
+      await $`adrafinil ${adrafinilAction} ${event.properties.info.id} --tool opencode`
     }
   }
 }
